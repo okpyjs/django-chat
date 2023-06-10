7c4e60f75ed906e7b61bcc0ef506c2938529812b
Oleksandr <oleksandrko448@gmail.com>
2023-06-10 21:40:16 +0300

    django channel authenticate with AuthMiddlewareStack

--- core/asgi.py
+++ core/asgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
@@ -19,6 +20,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        "websocket": URLRouter(chat.routing.websocket_urlpatterns),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(chat.routing.websocket_urlpatterns),
+        ),
     }
 )
