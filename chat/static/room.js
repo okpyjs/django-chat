d7d9a9f28bbeb37072a6a0f734b0b061131cf807
Oleksandr <oleksandrko448@gmail.com>
2023-06-11 04:27:20 +0300

    complete private message

--- chat/static/room.js
+++ chat/static/room.js
@@ -77,6 +77,12 @@ function connect() {
                 chatLog.value += data.user + " left the room.\n";
                 onlineUsersSelectorRemove(data.user);
                 break;
+            case "private_message":
+                chatLog.value += "PM from " + data.user + ": " + data.message + "\n";
+                break;
+            case "private_message_delivered":
+                chatLog.value += "PM to " + data.target + ": " + data.message + "\n";
+                break;
             default:
                 console.error("Unknown message type!");
                 break;
@@ -99,5 +105,12 @@ function connect() {
         }));
         chatMessageInput.value = "";
     };
+
+
 }
+onlineUsersSelector.onchange = function() {
+    chatMessageInput.value = "/pm " + onlineUsersSelector.value + " ";
+    onlineUsersSelector.value = null;
+    chatMessageInput.focus();
+};
 connect();
